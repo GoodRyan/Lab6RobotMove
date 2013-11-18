a6e2a845967bf934ace9dd602e7adba7321c2f97
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2013-11-18 15:31:03 -0700

    cleaned up main by moving some of the code into a function in the new implementation file. Also added a header.

--- main.c
+++ main.c
@@ -4,28 +4,15 @@
  * main.c
  */
 #include <msp430.h>
+#include "robotMovement.h"
 
 int main(void)
 {
     WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
 
-    P1DIR |= BIT2;
-    P1SEL |= BIT2;
+    initializePinOuts();
 
-    P1DIR |= BIT1;
-
-    TACTL &= ~(MC1|MC0);            // stop timer A0
-
-    TACTL |= TACLR;                // clear timer A0
-
-    TACTL |= TASSEL1;           // configure for SMCLK
-
-    TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
-    TACCR1 = 60;                // set duty cycle to 25/100 (25%)
-
-    TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-    TACTL |= MC0;                // count up
+    configureTimer();
 
     P1OUT |= BIT2;
     P1OUT &= ~BIT1;
---
+++ robotMovement.c
@@ -0,0 +1,25 @@
+#include "robotMovement.h"
+
+void initializePinOuts(){
+    P1DIR |= BIT2;
+    P1SEL |= BIT2;
+
+    P1DIR |= BIT1;
+    P2DIR |= BIT0;
+    P2DIR |= BIT1;
+}
+
+void configureTimer(){
+	TACTL &= ~(MC1|MC0);            // stop timer A0
+
+	    TACTL |= TACLR;                // clear timer A0
+
+	    TACTL |= TASSEL1;           // configure for SMCLK
+
+	    TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+	    TACCR1 = 60;                // set duty cycle to 25/100 (25%)
+
+	    TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+
+	    TACTL |= MC0;                // count up
+}
