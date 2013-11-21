3df4b5faa903c0cb77c5bee8af339c5aa2def019
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2013-11-21 10:31:49 -0700

    Robot functionality complete, avoid rough movement as this can lead to errors

--- main.c
+++ main.c
@@ -14,9 +14,19 @@ int main(void)
 
     configureTimer();
 
+    while(1){
+    moveRobotForward();
+    _delay_cycles(1000000);
+    moveRobotBackward();
+    _delay_cycles(1000000);
     turnRobotLeft();
-
-    while (1) {
+    _delay_cycles(300000);
+    turnRobotRight();
+    _delay_cycles(300000);
+    turnRobotLeft();
+    _delay_cycles(800000);
+    turnRobotRight();
+    _delay_cycles(800000);
     }
 
     return 0;
--- robotMovement.c
+++ robotMovement.c
@@ -5,49 +5,83 @@ void initializePinOuts(){
     P1SEL |= BIT2;
 
     P1DIR |= BIT1;
+    P1SEL |= BIT1;
+
     P2DIR |= BIT0;
+    P2SEL |= BIT0;
+
     P2DIR |= BIT1;
+    P2SEL |= BIT1;
 }
 
 void configureTimer(){
-	TACTL &= ~(MC1|MC0);            // stop timer A0
+	TA0CTL &= ~(MC1|MC0);            // stop timer A0
+	TA1CTL &= ~(MC1|MC0);            // stop timer A0
 
-	    TACTL |= TACLR;                // clear timer A0
+	TA0CTL |= TACLR;                // clear timer A0
+	TA1CTL |= TACLR;
 
-	    TACTL |= TASSEL1;           // configure for SMCLK
+	TA0CTL |= TASSEL1;           // configure for SMCLK
+	TA1CTL |= TASSEL1;
 
-	    TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
-	    TACCR1 = 60;                // set duty cycle to 25/100 (25%)
+	TA0CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+	TA0CCR1 = 50;
 
-	    TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+	TA1CCR0 = 100;
+	TA1CCR1 = 50;
 
-	    TACTL |= MC0;                // count up
-}
+	TA0CCTL0 |= OUTMOD2|OUTMOD0;
+	TA0CCTL1 |= OUTMOD2|OUTMOD0;
 
-void leftMotorForward(){
+	TA1CCTL0 |= OUTMOD2|OUTMOD0;
+	TA1CCTL1 |= OUTMOD2|OUTMOD0;
+
+	TA0CTL |= MC0;                // count up
+	TA1CTL |= MC0;
 
 }
 
 void leftMotorBackward(){
-    P1OUT |= BIT2;
-    P1OUT &= ~BIT1;
+	TA0CCTL1 &= ~OUTMOD1;
+	TA0CCTL1 |= OUTMOD2|OUTMOD0;
+	_delay_cycles(20000);
+	TA0CCTL0 &= ~OUTMOD0;
+	TA0CCTL0 |= OUTMOD2;
 }
 
-void rightMotorForward(){
-	P2OUT |= BIT0;
-	P2OUT &= ~BIT1;
+void leftMotorForward(){
+	TA0CCTL0 &= ~OUTMOD1;
+	TA0CCTL0 |= OUTMOD2|OUTMOD0;
+	_delay_cycles(20000);
+	TA0CCTL1 &= ~OUTMOD0;
+	TA0CCTL1 |= OUTMOD2;
 }
 
 void rightMotorBackward(){
+	TA1CCTL1 &= ~OUTMOD1;
+	TA1CCTL1 |= OUTMOD2|OUTMOD0;
+	_delay_cycles(20000);
+	TA1CCTL0 &= ~OUTMOD0;
+	TA1CCTL0 |= OUTMOD2;
 
+}
+
+void rightMotorForward(){
+	TA1CCTL0 &= ~OUTMOD1;
+	TA1CCTL0 |= OUTMOD2|OUTMOD0;
+	_delay_cycles(20000);
+	TA1CCTL1 &= ~OUTMOD0;
+	TA1CCTL1 |= OUTMOD2;
 }
 
 void moveRobotForward(){
-
+	leftMotorForward();
+	rightMotorForward();
 }
 
 void moveRobotBackward(){
-
+	leftMotorBackward();
+	rightMotorBackward();
 }
 
 void turnRobotLeft(){
@@ -56,5 +90,6 @@ void turnRobotLeft(){
 }
 
 void turnRobotRight(){
-
+	leftMotorForward();
+	rightMotorBackward();
 }
